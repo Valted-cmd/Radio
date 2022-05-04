1183344b900dbeb0d94ec10ce8cff16d45dc55fa
Adavletbaev <alex.davletbaev@yandex.ru>
2022-05-04 17:04:12 +0300

    main

---
+++ src/main/java/ru/netology/Radio.java
@@ -0,0 +1,59 @@
+package ru.netology;
+
+public class Radio {
+    private int currentStation;
+    private int currentVolume;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        this.currentVolume = currentVolume;
+    }
+
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0) {
+            currentStation = 9;
+        }
+
+        if (currentStation > 9) {
+            currentStation = 0;
+        }
+
+        this.currentStation = currentStation;
+    }
+
+    public void nextStation() {
+        if (currentStation >= 9) {
+            currentStation = 0;
+        } else {
+            currentStation = currentStation + 1;
+        }
+    }
+
+    public void prevStation() {
+        if (currentStation <= 0) {
+            currentStation = 9;
+        } else {
+            currentStation = currentStation - 1;
+        }
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void reduceVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+}
---
+++ src/test/java/ru/netology/RadioTest.java
@@ -0,0 +1,107 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RadioTest {
+    Radio radio = new Radio();
+
+    @Test
+    void setCurrentStation() {
+        radio.setCurrentStation(5);
+        assertEquals(5, radio.getCurrentStation());
+    }
+
+    @Test
+    void setCurrentStationLess0() {
+        radio.setCurrentStation(-1);
+        assertEquals(9, radio.getCurrentStation());
+    }
+
+    @Test
+    void setCurrentStationMoreThen9() {
+        radio.setCurrentStation(10);
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldnextStationIfCurrent0() {
+        radio.nextStation();
+        assertEquals(1, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldnextStationIfCurrent5() {
+        radio.setCurrentStation(5);
+        radio.nextStation();
+        assertEquals(6, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldnextStationIfCurrent9() {
+        radio.setCurrentStation(9);
+        radio.nextStation();
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldprevStationIfCurrent0() {
+        radio.prevStation();
+        assertEquals(9, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldprevStationIfCurrent5() {
+        radio.setCurrentStation(5);
+        radio.prevStation();
+        assertEquals(4, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldprevStationIfCurrent9() {
+        radio.setCurrentStation(9);
+        radio.prevStation();
+        assertEquals(8, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldincreaseVolumeIfCurrent0() {
+        radio.increaseVolume();
+        assertEquals(1, radio.getCurrentVolume());
+    }
+
+    @Test
+    void shouldincreaseVolumeIfCurrent5() {
+        radio.setCurrentVolume(5);
+        radio.increaseVolume();
+        assertEquals(6, radio.getCurrentVolume());
+    }
+
+    @Test
+    void shouldincreaseVolumeIfCurrent10() {
+        radio.setCurrentVolume(10);
+        radio.increaseVolume();
+        assertEquals(10, radio.getCurrentVolume());
+    }
+
+    @Test
+    void shouldreduceVolumeIfcurrent0() {
+        radio.reduceVolume();
+        assertEquals(0, radio.getCurrentVolume());
+    }
+
+    @Test
+    void shouldreduceVolumeIfcurrent5() {
+        radio.setCurrentVolume(5);
+        radio.reduceVolume();
+        assertEquals(4, radio.getCurrentVolume());
+    }
+
+    @Test
+    void shouldreduceVolumeIfcurrent10() {
+        radio.setCurrentVolume(10);
+        radio.reduceVolume();
+        assertEquals(9, radio.getCurrentVolume());
+    }
+}
